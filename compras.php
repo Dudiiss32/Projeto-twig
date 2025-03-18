010edce77bbcb9d788ae5e3976fb6c12918c37ca
Maria Eduarda Weber <mariaeduardaweber509@gmail.com>
2025-03-18 08:48:10 -0300

    Aula 3 - listagem compras

--- compras.php
+++ compras.php
@@ -1,7 +1,15 @@
 <?php
 
 require_once('twig_carregar.php');
+require('inc/banco.php');
+
+// Busca as compras no banco
+$dados = $pdo->query('SELECT * FROM compras');
+
+$comp = $dados->fetchAll(PDO::FETCH_ASSOC);
+
 
 echo $twig->render('compras.html', [
     'titulo' => 'Compras',
+    'compras' => $comp,
 ]);
---
+++ inc/banco.php
@@ -0,0 +1,4 @@
+<?php
+
+//Lebrete: Não se deve salvar senhas diretamente no arquivo (da maneira como estamos fazendo aqui)
+$pdo = new PDO('mysql:host=localhost;dbname=3info', 'root', '');
