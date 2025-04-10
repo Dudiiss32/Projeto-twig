8699202a0ea691fe0d13025aaba73327522ab286
Maria Eduarda Weber <mariaeduardaweber509@gmail.com>
2025-04-10 10:44:19 -0300

    compromissos filter

--- compromissos.php
+++ compromissos.php
@@ -9,15 +9,32 @@ date_default_timezone_set('America/Sao_Paulo');
 
 
 $id = $_GET['id'] ?? null;
+$filtro = $_GET['filtro'];
 
-if($id){
+if($filtro){
+    switch ($filtro) {
+        case 'crescente':
+            $dados = $pdo->query('SELECT * FROM compromissos ORDER BY data ASC');
+            $comp = $dados->fetchAll(PDO::FETCH_ASSOC);
+            echo $twig->render('compromissos.html', [
+                'compromissos'=> $comp
+            ]);
+            break;
+        case 'decrescente':
+            $dados = $pdo->query('SELECT * FROM compromissos ORDER BY data DESC');
+            $comp = $dados->fetchAll(PDO::FETCH_ASSOC);
+            echo $twig->render('compromissos.html', [
+                'compromissos'=> $comp
+            ]);
+            break;
+    }
+}else if($id){
     $dados = $pdo->prepare('SELECT * FROM compromissos WHERE id = :id');
     $dados->execute([':id' => $id]);
     $comp = $dados->fetch(PDO::FETCH_ASSOC);
     echo $twig->render('compromissos.html', [
         'compromissosEditar'=> $comp
     ]);
-    
 }else{
     $dados = $pdo->query('SELECT * FROM compromissos');
     $comp = $dados->fetchAll(PDO::FETCH_ASSOC);
@@ -25,5 +42,4 @@ if($id){
         'compromissos'=> $comp
     ]);
 }
-
 
